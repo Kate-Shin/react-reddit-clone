72d011c44c5c35cd54f76bdcf2a0297fb8a731b3
kateshin <sdk02307@gmail.com>
2023-01-02 23:37:12 +0900

    commit modify client

--- client/next.config.js
+++ client/next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     domains: [
       "www.gravatar.com",
       "localhost",
-      "ec2-43-200-8-24.ap-northeast-2.compute.amazonaws.com"
+      "ec2-3-38-244-187.ap-northeast-2.compute.amazonaws.com"
     ]
   }
 }
